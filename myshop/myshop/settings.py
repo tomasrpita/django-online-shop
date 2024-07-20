cb12e0bc4e7e11f62ffa60b868c4800145392f37
tomasrpita <tomasrpita@gmail.com>
2024-07-20 13:29:57 +0200

    chore: Update email server configuration and add flower to requirements

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,5 +134,13 @@ MEDIA_ROOT = BASE_DIR / "media/"
 
 CART_SESSION_ID = "cart"
 
+# Email server configuration
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# Email backend configuration . This is for testing purposes only.
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
