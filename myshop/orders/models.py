a4535db7aa7ffa3a3cfdf5b5f71714d0418803ab
tomasrpita <tomasrpita@gmail.com>
2024-07-26 07:18:11 +0200

    chore: Add coupon functionality to the project

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,6 +1,9 @@
+from decimal import Decimal
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.conf import settings
+
+from coupons.models import Coupon
 
 
 class Order(models.Model):
@@ -15,6 +18,14 @@ class Order(models.Model):
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
 
+    coupon = models.ForeignKey(
+        Coupon, related_name="orders", null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    discount = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+
     class Meta:
         ordering = ["-created"]
         indexes = [
@@ -24,8 +35,18 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id}"
 
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
