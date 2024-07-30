3b6131a68bd6139e0811ea593426ceb1f8c1bebd
tomasrpita <tomasrpita@gmail.com>
2024-07-30 06:30:53 +0200

    chore: Update URLs to support internationalization

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -19,8 +19,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path("cart/", include("cart.urls", namespace="cart")),
+#     path("orders/", include("orders.urls", namespace="orders")),
+#     path("payment/", include("payment.urls", namespace="payment")),
+#     path("coupons/", include("coupons.urls", namespace="coupons")),
+#     path("rosetta/", include("rosetta.urls")),
+#     path("", include("shop.urls", namespace="shop")),
+# ]
+
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("cart/", include("cart.urls", namespace="cart")),
     path("orders/", include("orders.urls", namespace="orders")),
@@ -28,7 +39,7 @@ urlpatterns = [
     path("coupons/", include("coupons.urls", namespace="coupons")),
     path("rosetta/", include("rosetta.urls")),
     path("", include("shop.urls", namespace="shop")),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
