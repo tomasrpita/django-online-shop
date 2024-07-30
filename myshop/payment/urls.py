b6c5080fe7c0eecd07ee322f76d3033de9454f71
tomasrpita <tomasrpita@gmail.com>
2024-07-30 06:47:09 +0200

    chore: Translate URL patterns and update form labels for quantity and coupon in cart and coupon forms

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views, webhooks
+from django.utils.translation import gettext_lazy as _
 
 app_name = "payment"
+# Traslated URL patterns will include a language prefix because they are included under i18n_patterns() in the main urls.py
 urlpatterns = [
-    path("process/", views.payment_process, name="process"),
-    path("completed/", views.payment_completed, name="completed"),
-    path("canceled/", views.payment_canceled, name="canceled"),
-    path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),
+    path(_("process/"), views.payment_process, name="process"),
+    path(_("completed/"), views.payment_completed, name="completed"),
+    path(_("canceled/"), views.payment_canceled, name="canceled"),
+    # path("webhook/", webhooks.stripe_webhook, name="stripe-webhook"),
 ]
